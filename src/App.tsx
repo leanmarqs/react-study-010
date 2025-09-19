f1202035aa6c4428db9163ce157eba2317229f70
leanmarqs <leanmarqs@gmail.com>
2025-09-19 09:19:05 -0300

    feat: Increment Button Implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
+import "./App.css"
+
 export function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-      the code goes here!
-    </>
+    <main className="container">
+      <h1 className="title">Count: {count}</h1>
+      <button className="btn" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </main>
   )
 }
